3b259856ecdfbceb9b1c9804880c82215736acaf
Michael Orlov <micorlov@gmail.com>
2026-07-13 22:52:29 +0300

    Refresh video poker cache

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
-const CACHE_NAME = 'vp-cache-v4';
+const CACHE_NAME = 'vp-cache-v5';
 const ASSETS = [
   'video_poker.html',
   'manifest.json',
