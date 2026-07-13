569c123fa8fdfbdd56a0138a795448b445b80c62
Michael Orlov <micorlov@gmail.com>
2026-07-13 22:49:20 +0300

    Restore poker card flip look

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
-const CACHE_NAME = 'vp-cache-v3';
+const CACHE_NAME = 'vp-cache-v4';
 const ASSETS = [
   'video_poker.html',
   'manifest.json',
