c86c95a3977a762ecb07219c597b934649701c2d
Michael Orlov <micorlov@gmail.com>
2026-07-22 23:55:56 +0300

    feat: add native Facebook sign-in for Android and iOS
    Wire up @capacitor-firebase/authentication's Facebook provider on both
    native platforms: Facebook SDK config (App ID, Client Token, protocol
    scheme) in Android manifest/strings/gradle vars, and FBSDKCoreKit
    ApplicationDelegate hooks + Info.plist keys on iOS. Verified building
    and running on the Android emulator with no crashes; the SDK's init
    ping to graph.facebook.com confirms the config loads correctly.

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Capacitor
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,6 +8,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
         window = UIWindow(frame: UIScreen.main.bounds)
         let bridgeVC = CAPBridgeViewController()
         window?.rootViewController = bridgeVC
@@ -39,7 +44,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
         // Called when the app was launched with a url. Feel free to add additional processing here,
         // but if you want the App API to support tracking app url opens, make sure to keep this call
-        return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
+        if ApplicationDelegate.shared.application(
+            app,
+            open: url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+        ) {
+            return true
+        } else {
+            return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
+        }
     }
 
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
